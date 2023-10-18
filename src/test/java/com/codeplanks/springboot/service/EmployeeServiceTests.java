040bb9cdc16622a3e28bb528c4bc4d8419ce78e7
Wasiu Idowu <hoxtygen@live.com>
2023-10-18 07:01:56 +0100

    Add saveEmployee method test that throws exception

--- src/test/java/com/codeplanks/springboot/service/EmployeeServiceTests.java
+++ src/test/java/com/codeplanks/springboot/service/EmployeeServiceTests.java
@@ -1,5 +1,6 @@
 package com.codeplanks.springboot.service;
 
+import com.codeplanks.springboot.exception.ResourceNotFoundException;
 import com.codeplanks.springboot.model.Employee;
 import com.codeplanks.springboot.repository.EmployeeRepository;
 import com.codeplanks.springboot.service.impl.EmployeeServiceImpl;
@@ -16,7 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
@@ -26,10 +30,11 @@ public class EmployeeServiceTests {
   @InjectMocks
   private EmployeeServiceImpl employeeService;
 
-  private  Employee employee;
+  private Employee employee;
+
   @BeforeEach
   public void setup() {
-     employee = Employee.builder()
+    employee = Employee.builder()
             .id(1L)
             .firstName("Theobroma")
             .lastName("Cacao")
@@ -42,7 +47,7 @@ public class EmployeeServiceTests {
   @Test
   public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
     // Given - precondition or setup
-   given(employeeRepository.findByEmail(employee.getEmail())).willReturn(
+    given(employeeRepository.findByEmail(employee.getEmail())).willReturn(
             Optional.empty());
     given(employeeRepository.save(employee)).willReturn(employee);
 
@@ -51,6 +56,23 @@ public class EmployeeServiceTests {
 
     // Then - verify the output
     Assertions.assertThat(savedEmployee).isNotNull();
+  }
+
+  // JUnit test for saveEmployee that throws exception
+  @DisplayName("save employee with exception")
+  @Test
+  public void givenExistingEmail_whenSaveEmployee_thenThrowsException() {
+    // Given - precondition or setup
+    given(employeeRepository.findByEmail(employee.getEmail())).willReturn(
+            Optional.of(employee));
+
+    // When - action or the behaviour we're testing for
+    org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+      employeeService.saveEmployee(employee);
+    });
+
+    // Then - verify the output
+    verify(employeeRepository, never()).save(any(Employee.class));
   }
 
 }
